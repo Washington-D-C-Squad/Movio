d2b4cb40ff72fbf38b71bbccc9fab7c0791850ec
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-30 21:49:49 +0300

    add : text style for movio design system add : Inter font

---
+++ designSystem/src/main/java/com/example/designsystem/textStyle/AppTheme.kt
@@ -0,0 +1,45 @@
+package com.example.designsystem.textStyle
+
+import MovioColors
+import MovioTextStyle
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.staticCompositionLocalOf
+import dark
+import defaultTextStyle
+import light
+
+@Stable
+object AppTheme {
+
+    val textStyle: MovioTextStyle
+        @Composable @ReadOnlyComposable get() = LocalMovioTextStyle.current
+
+    val colors: MovioColors
+        @Composable @ReadOnlyComposable get() = LocalMovioColors.current
+
+    @Composable
+    operator fun invoke(
+        useDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit
+    ) {
+        val colors = if (!useDarkTheme) {
+            light
+        } else {
+            dark
+        }
+
+        CompositionLocalProvider(
+            LocalMovioTextStyle provides defaultTextStyle,
+            LocalMovioColors provides colors
+        ) {
+            content()
+        }
+
+    }
+
+    private val LocalMovioColors = staticCompositionLocalOf { light }
+    private val LocalMovioTextStyle = staticCompositionLocalOf { defaultTextStyle }
+}
