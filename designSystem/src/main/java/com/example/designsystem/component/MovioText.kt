d9df44a9185767fd548331c6adb1174232f2f08f
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-01 20:29:45 +0300

    fix: MovioText by specify maxline and overflow

--- designSystem/src/main/java/com/example/designsystem/component/MovioText.kt
+++ designSystem/src/main/java/com/example/designsystem/component/MovioText.kt
@@ -1,18 +1,25 @@
 package com.example.designsystem.component
 
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 
 
 @Composable
 fun MovioText(
     modifier: Modifier = Modifier,
     text: String,
-    color:Color,
-    textStyle: TextStyle
+    color: Color,
+    textStyle: TextStyle,
+    maxLines: Int = Int.MAX_VALUE,
+    overflow: TextOverflow = TextOverflow.Ellipsis
 ) {
-    Text(text = text, style = textStyle.copy(color = color), modifier = modifier)
+     Text(
+         text = text, style = textStyle.copy(color = color),
+         modifier = modifier,maxLines=maxLines,overflow=overflow
+     )
 }
