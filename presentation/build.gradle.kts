d715bf5aed2149a56e07e18df759eb4d3d5ecf39
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-07-01 14:22:11 +0300

    add : compose dependency for presentation module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -30,10 +31,17 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    android {
+        buildFeatures {
+            compose = true
+        }
+    }
 }
 
 dependencies {
-
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation("androidx.compose.foundation:foundation")
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
