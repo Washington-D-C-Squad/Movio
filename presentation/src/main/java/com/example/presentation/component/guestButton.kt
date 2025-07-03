b4722f171875b1e5932c2d2f91e2f766fc54b1af
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-04 00:55:41 +0300

    add: Arabic localization for GuestButton

--- presentation/src/main/java/com/example/presentation/component/guestButton.kt
+++ presentation/src/main/java/com/example/presentation/component/guestButton.kt
@@ -7,16 +7,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.AppTheme
 import com.example.designsystem.component.MovioButton
 import com.example.designsystem.component.MovioText
+import com.example.presentation.R
 
 @Composable
 fun GuestButton(
     modifier: Modifier = Modifier,
     color: Color = Color.Transparent,
-    text: String = "Continue as a guest",
+    text: String = stringResource(R.string.continue_as_a_guest),
     onClick: () -> Unit,
 ) {
 
