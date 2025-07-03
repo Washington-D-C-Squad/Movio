06e19b6b357f6893b1bb0d546f69dcf7229dd8df
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-04 00:44:31 +0300

    refactor: rename MaxWidthButton to ActionButton and update GuestButton implementation

--- presentation/src/main/java/com/example/presentation/component/ActionButton.kt
+++ presentation/src/main/java/com/example/presentation/component/ActionButton.kt
@@ -1,43 +1,46 @@
 package com.example.presentation.component
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import com.example.designsystem.AppTheme
+import com.example.designsystem.R
 import com.example.designsystem.component.MovioButton
+import com.example.designsystem.component.MovioIcon
 import com.example.designsystem.component.MovioText
 
 @Composable
-fun GuestButton(
+fun ActionButton(
     modifier: Modifier = Modifier,
-    color: Color = Color.Transparent,
-    text: String = "Continue as a guest",
+    color: Color = AppTheme.colors.brandColors.primary,
+    isLoading: Boolean = false,
+    text: String,
+    icon: Painter = painterResource(R.drawable.loading),
     onClick: () -> Unit,
 ) {
-
-
     MovioButton(
         modifier = modifier
             .padding(horizontal = AppTheme.spacing.medium)
-
-            .fillMaxWidth()
-            .border(
-                width = 1.dp,
-                color = AppTheme.colors.surfaceColor.onSurface_3,
-                shape = RoundedCornerShape(AppTheme.radius.xxLarge)
-            ),
+            .fillMaxWidth(),
         color = color,
         onClick = onClick
     ) {
+        if (isLoading) {
+            MovioIcon(
+                painter = icon,
+                contentDescription = "loading icon",
+                tint = AppTheme.colors.brandColors.onPrimary,
+                modifier = Modifier.padding(end = AppTheme.spacing.extraSmall)
+            )
+        }
         MovioText(
             modifier = Modifier.padding(vertical = AppTheme.spacing.medium),
             text = text,
-            color = AppTheme.colors.surfaceColor.onSurface,
+            color = AppTheme.colors.brandColors.onPrimary,
             textStyle = AppTheme.textStyle.label.medium14,
         )
     }
