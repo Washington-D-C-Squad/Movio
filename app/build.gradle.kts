b90b0704128eac8defedeaf17155bafa97fe5a86
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-03 15:31:12 +0300

    add: enable buildConfig in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,33 +50,35 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
+
     }
-}
 
-dependencies {
+    dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    //firebase
-    implementation("com.google.firebase:firebase-analytics")
-    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
-    implementation("com.google.firebase:firebase-crashlytics-ndk")
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(project(":designSystem"))
-    implementation(project(":detectImageContent"))
-    implementation(project(":domain"))
-    implementation(project(":data"))
-    implementation(project(":presentation"))
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.ui)
+        implementation(libs.androidx.ui.graphics)
+        implementation(libs.androidx.ui.tooling.preview)
+        implementation(libs.androidx.material3)
+        //firebase
+        implementation("com.google.firebase:firebase-analytics")
+        implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+        implementation("com.google.firebase:firebase-crashlytics-ndk")
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.ui.test.junit4)
+        debugImplementation(libs.androidx.ui.tooling)
+        debugImplementation(libs.androidx.ui.test.manifest)
+        implementation(project(":designSystem"))
+        implementation(project(":detectImageContent"))
+        implementation(project(":domain"))
+        implementation(project(":data"))
+        implementation(project(":presentation"))
+    }
 }
