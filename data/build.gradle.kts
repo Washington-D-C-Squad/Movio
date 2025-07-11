db92b2f1ecb2384091881b1b525f34333aea9ba3
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-12 02:52:29 +0300

    fix: downgrade Gson to 2.10.1
    The Gson version has been downgraded from 2.11.0 to 2.10.1 in the `data/build.gradle.kts` file. This change was likely made to address a compatibility issue or a bug present in the newer version.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(project(":domain"))
-    implementation ("com.google.code.gson:gson:2.11.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
 
     implementation("io.ktor:ktor-client-core:2.3.13")
     implementation("io.ktor:ktor-client-cio:2.3.13")
