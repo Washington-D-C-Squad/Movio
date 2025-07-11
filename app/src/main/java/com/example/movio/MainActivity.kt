f8a83d6a596be8b903bfb35cfa84a0ee9204d52c
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-12 02:02:31 +0300

    style: Remove SearchScreen and update theme
    This commit removes the SearchScreen from MainActivity and updates the theme to use AppTheme instead of MovioTheme.

--- app/src/main/java/com/example/movio/MainActivity.kt
+++ app/src/main/java/com/example/movio/MainActivity.kt
@@ -1,19 +1,18 @@
-package com.example.movio
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.movio.screens.SearchScreen
-import com.example.movio.ui.theme.MovioTheme
+import com.example.designsystem.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovioTheme {
+            AppTheme (){
+
             }
+
         }
     }
 }
