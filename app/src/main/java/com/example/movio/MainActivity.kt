654741348c05fae1436438c25650f92dffd92526
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-12 08:26:56 +0300

    style: update MainActivity theme to AppTheme
    This commit updates the MainActivity to use `AppTheme` from the `designsystem` module instead of the local `MovioTheme`.

--- app/src/main/java/com/example/movio/MainActivity.kt
+++ app/src/main/java/com/example/movio/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.movio.screens.SearchScreen
-import com.example.movio.ui.theme.MovioTheme
+import com.example.designsystem.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovioTheme {
+            AppTheme (){
+
             }
         }
     }
