fa25941bba060c7a382ea86455b96f19478fb54a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-14 18:03:36 +0300

    feat: inject context to local datasource

--- data/src/main/java/com/madrid/data/dataSource/local/SearchLocalDataSource.kt
+++ data/src/main/java/com/madrid/data/dataSource/local/SearchLocalDataSource.kt
@@ -1,33 +1,35 @@
 package com.madrid.data.dataSource.local
 
+import android.content.Context
+import com.madrid.data.dataSource.local.entity.ArtistEntity
 import com.madrid.data.dataSource.local.entity.MovieEntity
+import com.madrid.data.dataSource.local.entity.SeriesEntity
 import com.madrid.data.repositories.SearchLocalSource
 import com.madrid.data.repositories.mappers.toMovieEntity
 import com.madrid.domain.entity.Movie
 import kotlinx.coroutines.flow.Flow
 
 class SearchLocalDataSource(
-
+    private val context: Context
 ) : SearchLocalSource {
 
 
-    val dao = MovioDatabase.Companion.getInstanceWithoutContext()
+    private val dao = MovioDatabase.Companion.getInstance(context)
 
     suspend fun insertMovie(movie: Movie) {
-        dao.movieDao().insertMovie(
-            movie.toMovieEntity()
-        )
+        val entity = movie.toMovieEntity()
+        dao.movieDao().insertMovie(entity)
     }
 
     override fun getMoviesByTitle(query: String): Flow<List<MovieEntity>> {
         return dao.movieDao().getMovieByTitle("%$query%")
     }
 
-    override fun getSeriesByTitle(query: String): Flow<List<MovieEntity>> {
+    override fun getSeriesByTitle(query: String): Flow<List<SeriesEntity>> {
         return dao.seriesDao().getSeriesByTitle("%$query%")
     }
 
-    override fun getArtistsByTitle(query: String): Flow<List<MovieEntity>> {
+    override fun getArtistsByTitle(query: String): Flow<List<ArtistEntity>> {
         return dao.artistDao().getArtistByName("%$query%")
     }
 
