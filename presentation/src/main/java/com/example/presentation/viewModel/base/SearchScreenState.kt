2568c5092e7e882aa055eb8e9fcd6d708898cdf2
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-14 14:31:29 +0300

    feat : implement SearchScreenState

---
+++ presentation/src/main/java/com/example/presentation/viewModel/base/SearchScreenState.kt
@@ -0,0 +1,37 @@
+package com.example.presentation.viewModel.base
+
+import com.example.domain.RecentSearchItem
+
+
+data class SearchScreenState(
+    val searchUiState: SearchUiState = SearchUiState(),
+    val recentSearchUiState: List<RecentSearchItem> = emptyList(),
+
+    //TODO add the remain ui state
+) {
+    data class SearchUiState(
+        val forYouMovies: List<MovieUiState> = listOf(MovieUiState()),
+        val exploreMoreMovies: List<MovieUiState> = listOf(MovieUiState()),
+        val searchResults: List<MovieUiState> = listOf(MovieUiState()),
+        val isLoading: Boolean = false,
+        val errorMessage: String? = null
+    )
+    data class MovieUiState(
+        val id: String="",
+        val title: String="",
+        val imageUrl: String="",
+        val rating: String="",
+        val category: String=""
+    )
+    data class Serase(
+        val id: String,
+        val title: String,
+        val imageUrl: String,
+        val rating: String,
+        val category: String
+    )
+    data class RecentSearchUiState (
+        val recentSearch : List<String> = emptyList()
+    )
+    //TODO add the remain ui state
+}
