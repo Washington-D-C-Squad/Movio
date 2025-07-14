78b14f9c4e4e010de80d0e083122a03ded0d21df
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-14 19:10:54 +0300

    Update SearchListViewModel.kt

--- presentation/src/main/java/com/madrid/presentation/component/screens/searchscreen/SearchListViewModel.kt
+++ presentation/src/main/java/com/madrid/presentation/component/screens/searchscreen/SearchListViewModel.kt
@@ -1,11 +1,11 @@
 package com.madrid.presentation.component.screens.SearchScreen
 
-import androidx.lifecycle.ViewModel
 import com.madrid.data.InMemoryRecentSearchRepository
 import com.madrid.domain.RecentSearchItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import androidx.lifecycle.ViewModel
 
 class SearchListViewModel(private val repo: InMemoryRecentSearchRepository) : ViewModel() {
     private val _recentSearches = MutableStateFlow<List<RecentSearchItem>>(emptyList())
