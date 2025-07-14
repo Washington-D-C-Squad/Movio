6d636e409d9c021e54c3d4cec277984dcc55dc1a
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-15 00:26:27 +0300

    refactor : navigation

--- presentation/src/main/java/com/madrid/presentation/navigation/route/HomeRoute.kt
+++ presentation/src/main/java/com/madrid/presentation/navigation/route/HomeRoute.kt
@@ -3,10 +3,11 @@ package com.madrid.presentation.navigation.route
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.madrid.presentation.navigation.FakeHomeScreen
 import com.madrid.presentation.navigation.Screen
 
 fun NavGraphBuilder.homeRoute(navController: NavController) {
     composable(route = Screen.Home.route) {
-
+        FakeHomeScreen()
     }
 }
