e89628c4deded0476b9ba08bc945fbf34f4b917d
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-15 16:05:02 +0300

    init: FilteredScreen

---
+++ presentation/src/main/java/com/madrid/presentation/screens/searchScreen/FillteredSearchScreen.kt
@@ -0,0 +1,58 @@
+package com.madrid.presentation.screens.searchScreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.madrid.designsystem.AppTheme
+import com.madrid.designsystem.component.MovioIcon
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun FilteredScreen(
+    viewModel: SearchViewModel = koinViewModel()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        val uiState by viewModel.uiState.collectAsState()
+
+        ContentFilteredScreen()
+
+        uiState.errorMessage?.let { errorMsg ->
+            LaunchedEffect(errorMsg) {
+                // handle error
+            }
+
+
+            if (uiState.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    MovioIcon(
+                        painter = painterResource(com.madrid.designsystem.R.drawable.loading),
+                        contentDescription = "Loading",
+                        tint = AppTheme.colors.brandColors.primary
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun ContentFilteredScreen() {
+
+}
