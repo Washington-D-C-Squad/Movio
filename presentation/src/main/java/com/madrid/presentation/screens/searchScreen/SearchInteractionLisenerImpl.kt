bc244287ec139b5c73ffbcdf800f76ee0ace015d
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-16 15:39:04 +0300

    fix : onSearchQuerySubmitted

--- presentation/src/main/java/com/madrid/presentation/screens/searchScreen/SearchInteractionLisenerImpl.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/searchScreen/SearchInteractionLisenerImpl.kt
@@ -2,10 +2,13 @@ package com.madrid.presentation.screens.searchScreen
 
 
 val interactionListener = object : SearchInteractionListener {
-    override fun onSearchQuerySubmitted(query: String,viewModel:SearchViewModel) {
-        viewModel.searchFilteredMovies(query)
-        viewModel.searchSeries(query)
-        viewModel.artists(query)
-        viewModel.topResult(query)
+    override fun onSearchQuerySubmitted(query: String, viewModel: SearchViewModel
+    ) {
+        if (query.isNotBlank()) {
+            viewModel.searchFilteredMovies(query)
+            viewModel.searchSeries(query)
+            viewModel.artists(query)
+            viewModel.topResult(query)
+        }
     }
 }
