75d57c3caffab5453d14c534d1974000021c5107
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-17 19:59:57 +0300

    refactor : use stringresource

---
+++ presentation/src/main/java/com/madrid/presentation/screens/searchScreen/SearchResultMessage.kt
@@ -0,0 +1,18 @@
+package com.madrid.presentation.screens.searchScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.madrid.designsystem.AppTheme
+import com.madrid.designsystem.component.MovioText
+import com.madrid.presentation.R
+
+@Composable
+fun SearchResultMessage(items: String, modifier: Modifier = Modifier) {
+    MovioText(
+        stringResource(id = R.string.search_result_count, items),
+        AppTheme.colors.surfaceColor.onSurfaceVariant,
+        AppTheme.textStyle.label.smallRegular14,
+        modifier = modifier
+    )
+}
