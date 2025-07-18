9bd65f12a3bc3ab1ab77343ce5d294781c2f2927
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-18 21:25:38 +0300

    feat: ActorOverlay composable
    This commit introduces a new composable function `ActorOverlay`.
    
    This composable displays an actor's image within a circular frame. It includes a shadow effect and a colored border. The image is loaded asynchronously using Coil.

---
+++ presentation/src/main/java/com/madrid/presentation/composables/movieActorBackround/ActorOverlay.kt
@@ -0,0 +1,43 @@
+package com.madrid.presentation.composables.movieActorBackround
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun BoxScope.ActorOverlay(actorImageUrl: String, borderColor: Color) {
+    Box(
+        modifier = Modifier
+            .size(120.dp)
+            .align(Alignment.Center)
+            .shadow(8.dp, CircleShape, clip = false)
+            .clip(CircleShape)
+            .border(4.dp, borderColor.copy(alpha = 0.7f), CircleShape)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(actorImageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(CircleShape)
+        )
+    }
+} 
