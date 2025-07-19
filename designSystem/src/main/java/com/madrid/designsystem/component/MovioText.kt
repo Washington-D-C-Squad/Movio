505b97e036c4c6034eb43487dc50e25cad95d646
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-19 04:48:46 +0300

    feat: Added Movie Description Component
    This commit introduces a new `MovieDescription` Composable function.
    
    Key features:
    - Displays movie descriptions with a "Read More" / "Read Less" functionality for long descriptions.
    - Truncates descriptions to 150 characters by default, expanding to full text when "Read More" is clicked.
    - Includes a preview for the `MovieDescription` component.
    
    Additionally, this commit:
    - Creates a `MovieDetailsDemoScreen` to showcase the `MovieDescription` and other movie details components with sample data.
    - Implements a `MovieDetailsNavigationHeader` Composable for back, share, and like actions.
    - Updates `MovioText` and `MovioIcon` to use `BasicText` and `Image` respectively.
    - Adjusts padding in `BottomMediaActions`.

--- designSystem/src/main/java/com/madrid/designsystem/component/MovioText.kt
+++ designSystem/src/main/java/com/madrid/designsystem/component/MovioText.kt
@@ -1,6 +1,6 @@
 package com.madrid.designsystem.component
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,7 +17,7 @@ fun MovioText(
     maxLines: Int = Int.MAX_VALUE,
     overflow: TextOverflow = TextOverflow.Ellipsis
 ) {
-    Text(
+    BasicText(
         text = text, style = textStyle.copy(color = color),
         modifier = modifier, maxLines = maxLines, overflow = overflow
     )
@@ -32,7 +32,7 @@ fun MovioTextHighlight(
     maxLines: Int = Int.MAX_VALUE,
     overflow: TextOverflow = TextOverflow.Ellipsis
 ) {
-    Text(
+    BasicText(
         text = text, style = textStyle.copy(color = color),
         modifier = modifier, maxLines = maxLines, overflow = overflow
     )
