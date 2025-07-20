f1fcf3e4e8d120d24498095e13637c9c66659190
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-20 19:30:36 +0300

    fix: unknown exception import

--- data/src/main/java/com/madrid/data/dataSource/remote/utils/ResponseWrapper.kt
+++ data/src/main/java/com/madrid/data/dataSource/remote/utils/ResponseWrapper.kt
@@ -1,13 +1,17 @@
 package com.madrid.data.dataSource.remote.utils
 
 import com.madrid.domain.exceptions.InvalidRequestException
+import com.madrid.domain.exceptions.ServerErrorException
+import com.madrid.domain.exceptions.TimeoutException
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.statement.HttpResponse
-import io.ktor.utils.io.errors.IOException
 import com.madrid.domain.exceptions.UnauthorizedException
+import com.madrid.domain.exceptions.UnknownException
+import io.ktor.client.plugins.RedirectResponseException
+import io.ktor.client.plugins.ServerResponseException
+import io.ktor.utils.io.errors.IOException
 
 suspend inline fun <reified T> responseWrapper(
     client: HttpClient,
@@ -19,10 +23,13 @@ suspend inline fun <reified T> responseWrapper(
         when (e.response.status.value) {
             400 -> throw InvalidRequestException(message = "Invalid Request Method: ${e.message}")
             403 -> throw UnauthorizedException()
-            408 -> throw com.madrid.domain.exceptions.TimeoutException(
+            408 -> throw TimeoutException(
                 message = "Timeout Error: ${e.message}"
             )
-            else -> throw UnknownError()
+
+            else -> throw UnknownException(e.message)
         }
+    } catch (e: Exception) {
+        throw UnknownException(message = "Unknown error occurred: ${e.message}")
     }
 }
