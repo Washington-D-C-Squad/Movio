571a850af594e5d23338b6bf6f9e6b3b2663d4bb
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-20 07:27:17 +0300

    refactor : add preview dependency

--- designSystem/build.gradle.kts
+++ designSystem/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation(libs.androidx.ui.tooling.preview)
+
     //material3
     implementation(libs.material3)
     implementation(libs.androidx.material3.window.size.class1)
