cb5e21af18b6d829a95e985ab7f0015889df510f
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-20 19:29:24 +0300

    refactor: rename custom exception class

--- domain/src/main/java/com/madrid/domain/exceptions/MovieExceptions.kt
+++ domain/src/main/java/com/madrid/domain/exceptions/MovieExceptions.kt
@@ -1,17 +1,17 @@
 package com.madrid.domain.exceptions
 
-open class MovioExceptions(message: String) : Exception(message)
+open class MovioException(message: String) : Exception(message)
 
 
-class UnknownException(message: String) : MovioExceptions(message)
+class UnknownException(message: String) : MovioException(message)
 
 // Authorization exceptions
-open class AuthorizationException(message: String) : MovioExceptions(message)
+open class AuthorizationException(message: String) : MovioException(message)
 class UnauthorizedException : AuthorizationException(message = "Unauthorized")
 class ValidationException : AuthorizationException(message = "Validation")
 
 // Remote source exceptions
-open class ServerException(message: String) : MovioExceptions(message)
+open class ServerException(message: String) : MovioException(message)
 class ServerErrorException : ServerException(message = "Server Error")
 class InvalidRequestException(message: String) :
     ServerException(message = "Invalid Request Method: $message")
@@ -19,8 +19,8 @@ class InvalidRequestException(message: String) :
 class InvalidResponseException(message: String) : ServerException(message = "Invalid Response: $message")
 class TimeoutException(message: String) : ServerException(message = "Timeout Error: $message")
 
-open class NetworkException(message: String) : MovioExceptions(message)
+open class NetworkException(message: String) : MovioException(message)
 class NetworkErrorException(message: String) : NetworkException(message = "Network Error: $message")
 
 // Domain exceptions
-class NotFoundException(message: String) : MovioExceptions(message = "Not Found: $message")
+class NotFoundException(message: String) : MovioException(message = "Not Found: $message")
