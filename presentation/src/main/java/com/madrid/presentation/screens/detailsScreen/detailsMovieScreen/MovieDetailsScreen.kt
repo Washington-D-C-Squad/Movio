79cd39048ae7b5bf834e9b12eb37621dfde48dfd
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-21 01:22:23 +0300

    Refactor: Remove unused demo screen and data class
    This commit removes the `MovieDetailsDemoScreen.kt` file, which contained a composable function used for previewing the movie details screen with sample data.
    
    The `MovieDetailsData` data class, previously used to structure this sample data, has also been removed from `MovieDetailsScreen.kt` as it is no longer needed. The preview functionality previously relying on this has been commented out.

--- presentation/src/main/java/com/madrid/presentation/screens/detailsScreen/detailsMovieScreen/MovieDetailsScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/detailsScreen/detailsMovieScreen/MovieDetailsScreen.kt
@@ -25,20 +25,6 @@ import com.madrid.presentation.component.movieActorBackground.MoviePosterDetailS
 import com.madrid.presentation.viewModel.detailsViewModel.DetailsMovieViewModel
 import org.koin.androidx.compose.koinViewModel
 
-data class MovieDetailsData(
-    val id: String,
-    val title: String,
-    val originalTitle: String,
-    val posterUrl: String,
-    val backdropUrl: String,
-    val genres: List<String>,
-    val rating: String,
-    val duration: String,
-    val releaseDate: String,
-    val description: String,
-    val castMembers: List<CastMember>
-)
-
 @Composable
 fun MovieDetailsScreen(
     modifier: Modifier = Modifier,
@@ -116,46 +102,3 @@ fun MovieDetailsScreen(
 fun TextWithReadMore(description: String, modifier: Modifier) {
     TODO("Not yet implemented")
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun MovieDetailsScreenPreview() {
-//    MovioTheme {
-//        MovieDetailsScreen(
-//            movieData = MovieDetailsData(
-//                id = "1",
-//                title = "Ballerina",
-//                originalTitle = "BALLERINA",
-//                posterUrl = "https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-//                backdropUrl = "https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-//                genres = listOf("Action", "Thriller", "Crime"),
-//                rating = "4.5",
-//                duration = "2h 5min",
-//                releaseDate = "06/06/2025",
-//                description = "Taking place during the events of John Wick: Chapter 3 – Parabellum, Eve Macarro begins her training in the assassin traditions of the Ruska Roma. Derek Kolstad :Characters , Len Wiseman:Director , Shay Hatten",
-//                castMembers = listOf(
-//                    CastMember(
-//                        id = "1",
-//                        name = "Ana de Armas",
-//                        imageUrl = "https://image.tmdb.org/t/p/w500/3vxvsmYLTf4jnr163SUlBIWX8qx.jpg"
-//                    ),
-//                    CastMember(
-//                        id = "2",
-//                        name = "Keanu Reeves",
-//                        imageUrl = "https://image.tmdb.org/t/p/w500/4D0PpNI0km5B9Gk7SZOo6hJxJ9P.jpg"
-//                    ),
-//                    CastMember(
-//                        id = "3",
-//                        name = "Ian McShane",
-//                        imageUrl = "https://image.tmdb.org/t/p/w500/9H7oVx4b6Z0j3EjLZN9mzcqcJjU.jpg"
-//                    ),
-//                    CastMember(
-//                        id = "4",
-//                        name = "Lance Reddick",
-//                        imageUrl = "https://image.tmdb.org/t/p/w500/8i6ZDkX1s6tB3iJ9uPxQqR6ZqJ9P.jpg"
-//                    )
-//                )
-//            )
-//        )
-//    }
-//}
