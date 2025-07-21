113573613ae642b941ffa4aac3e0efd29d51cb4c
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-07-21 11:53:36 +0300

    build/add dependencies of retrofit,converter gson , logging interceptor

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -84,4 +84,9 @@ dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
     }
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
 }
