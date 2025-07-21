705325dbfb28f1ed3b454a4a2c5d476563c60bff
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-07-21 13:05:54 +0300

    refactor:refactor RemoteDataSourceImpl.kt to using MovieApi and refactor di to inject MovieApi

--- data/src/main/java/com/madrid/data/DiModule.kt
+++ data/src/main/java/com/madrid/data/DiModule.kt
@@ -1,5 +1,8 @@
 package com.madrid.data
 
+import com.madrid.data.BuildConfig.BASE_URL
+import com.madrid.data.dataSource.remote.MovieApi
+import com.madrid.data.dataSource.remote.MovieInterceptor
 import com.madrid.data.repositories.remote.RemoteDataSource
 import com.madrid.data.dataSource.remote.RemoteDataSourceImpl
 import io.ktor.client.HttpClientConfig
@@ -7,10 +10,35 @@ import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.CIOEngineConfig
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val roomModule = module {
-    single<RemoteDataSource> { RemoteDataSourceImpl(get(), get()) }
+
+    single<Interceptor> { MovieInterceptor() }
+
+    single { OkHttpClient.Builder().addInterceptor(get<Interceptor>()).build() }
+
+    single<String>(named("baseUrl")) {
+        val apiVersion = "/3/"
+        val urlProtocol = "https://"
+        urlProtocol + BASE_URL + apiVersion
+    }
+
+    single {
+        val retrofit :Retrofit = Retrofit.Builder()
+            .baseUrl(get<String>(named("baseUrl")))
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(MovieApi::class.java)
+    }
+    single<RemoteDataSource> { RemoteDataSourceImpl(get()) }
     single { Json { ignoreUnknownKeys = true } }
     single { CustomHttpClient() }
     single<HttpClientEngine> { CIO.create() }
