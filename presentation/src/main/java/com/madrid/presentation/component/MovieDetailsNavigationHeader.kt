87c2baa252dcd6b0b039df60d2118cf4f0f03cea
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-21 20:26:12 +0300

    Test: Add Preview for MovieDetailsNavigationHeader
    A new Composable function, `MovieDetailsNavigationHeaderPreview`, has been added. This function is annotated with `@Preview(showBackground = true, showSystemUi = true)` and `@Composable` to enable previewing the `MovieDetailsNavigationHeader` component in Android Studio.

--- presentation/src/main/java/com/madrid/presentation/component/MovieDetailsNavigationHeader.kt
+++ presentation/src/main/java/com/madrid/presentation/component/MovieDetailsNavigationHeader.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.madrid.designSystem.R
 import com.madrid.designSystem.component.MovioIcon
@@ -90,4 +91,9 @@ fun MovieDetailsNavigationHeader(
             }
         }
     }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MovieDetailsNavigationHeaderPreview() {
+    MovieDetailsNavigationHeader()
 }
