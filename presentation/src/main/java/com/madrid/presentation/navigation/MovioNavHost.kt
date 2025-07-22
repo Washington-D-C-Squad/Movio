ea620614b8cd5ae6aef572fd018b58bb1ffdea72
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-07-22 17:44:21 +0300

    add : composable in MovioNavHost

--- presentation/src/main/java/com/madrid/presentation/navigation/MovioNavHost.kt
+++ presentation/src/main/java/com/madrid/presentation/navigation/MovioNavHost.kt
@@ -1,17 +1,31 @@
 package com.madrid.presentation.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.madrid.presentation.screens.SeeAllForYou.SeeAllForYouScreen
+import com.madrid.presentation.screens.detailsScreen.ActorDetails
 import com.madrid.presentation.screens.searchScreen.SearchScreen
 
 @Composable
-fun MovioNavHost(navController: NavHostController){
+fun MovioNavHost(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Destinations.HomeScreen
+        startDestination = Destinations.HomeScreen,
+        enterTransition = {
+            fadeIn(tween(0))
+        },
+        exitTransition = {
+            fadeOut(tween(0))
+        }
     ) {
+        composable<Destinations.SeeAllForYouScreen> {
+            SeeAllForYouScreen()
+        }
         composable<Destinations.SplashScreen> {
             //call SplashScreen()
         }
@@ -25,7 +39,7 @@ fun MovioNavHost(navController: NavHostController){
             SearchScreen()
         }
         composable<Destinations.HomeScreen> {
-           FakeHomeScreen()
+            FakeHomeScreen()
         }
         composable<Destinations.MovieDetailsScreen> {
             //call MovieDetailsScreen()
@@ -45,6 +59,15 @@ fun MovioNavHost(navController: NavHostController){
         composable<Destinations.EpisodesScreen> {
             //call EpisodesScreen()
         }
-
+        composable<Destinations.LibraryScreen> {
+            FakeLibraryScreen()
+        }
+        composable<Destinations.MoreScreen> {
+            FakeMoreScreen()
+        }
+        composable("actor_details/{actorId}") { backStackEntry ->
+            val actorId = backStackEntry.arguments?.getString("actorId") ?: return@composable
+            ActorDetails(actorId = actorId)
+        }
     }
 }
