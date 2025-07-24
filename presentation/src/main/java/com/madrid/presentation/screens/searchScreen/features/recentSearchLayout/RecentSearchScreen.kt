c4c27ebd52e768b400a74774ad9e837a45559588
karrar-abbas <karar.abbas.ch@gmail.com>
2025-07-24 16:31:38 +0300

    refactor: delete preview for recent search screen

--- presentation/src/main/java/com/madrid/presentation/screens/searchScreen/features/recentSearchLayout/RecentSearchScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/searchScreen/features/recentSearchLayout/RecentSearchScreen.kt
@@ -3,20 +3,15 @@ package com.madrid.presentation.screens.searchScreen.features.recentSearchLayout
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridScope
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import com.madrid.designSystem.component.MovioText
 import com.madrid.designSystem.theme.Theme
 import com.madrid.presentation.R
@@ -65,26 +60,5 @@ fun LazyGridScope.recentSearchScreen(
             onRemoveClick = { onRemoveItem(searchHistory[searchItem]) },
             highlightCharactersInText = highlightCharactersInText,
         )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RecentSearchScreenPreview() {
-    val sampleHistory = listOf("Compose", "Android", "Jetpack", "Kotlin")
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize()
-    ) {
-        /* put high light recent screen text fun that is in viewmodel */
-
-//        recentSearchScreen(
-//            searchHistory = sampleHistory,
-//            onSearchItemClick = { /* preview click */ },
-//            onRemoveItem = { /* preview remove */ },
-//            onClearAll = { /* preview clear */ },
-//            searchQuery = "",
-//            highlightCharactersInText =
-//        )
     }
 }
