94fbfb73c2f086c8c370ef2f22635881142cf5d0
yousef_osama11 <oyousef709@gmail.com>
2025-07-24 15:49:50 +0300

    refactor series details screen

--- presentation/src/main/java/com/madrid/presentation/screens/detailsScreen/seriesDetails/SeriesDetailsScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/detailsScreen/seriesDetails/SeriesDetailsScreen.kt
@@ -2,10 +2,13 @@ package com.madrid.presentation.screens.detailsScreen.seriesDetails
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -15,11 +18,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.madrid.designSystem.component.MovioText
 import com.madrid.designSystem.component.TopAppBar
 import com.madrid.designSystem.theme.Theme
+import com.madrid.presentation.R
 import com.madrid.presentation.component.BottomMediaActions
 import com.madrid.presentation.component.CastMember
 import com.madrid.presentation.component.TopCastSection
@@ -97,17 +103,32 @@ fun SeriesDetailsScreen(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
             )
             Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                MovioText(
+                    text = "ٍCurrent Seasons",
+                    color = Theme.color.surfaces.onSurface,
+                    textStyle = Theme.textStyle.headline.mediumMedium18
+                )
+
+                MovioText(
+                    text = "${stringResource(id = R.string.see_all)} >",
+                    color = Theme.color.surfaces.onSurfaceVariant,
+                    textStyle = Theme.textStyle.label.smallRegular14,
+                    modifier = Modifier.clickable { navController.navigate(Destinations.SeasonsScreen(uiState.seriesId,1)) }
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
             ReviewScreen(
                 onSeeAllReviews = {},
                 uiState = ReviewsScreenUiState()
             )
             Spacer(modifier = Modifier.height(32.dp))
-            MovioText(
-                text = "See all >",
-                color = Theme.color.surfaces.onSurfaceVariant,
-                textStyle = Theme.textStyle.label.smallRegular14,
-                modifier = Modifier.clickable { navController.navigate(Destinations.SeasonsScreen(uiState.seriesId,1)) }
-            )
             SimilarSeriesSection(
                 onSeeAllClick = {},
                 onSeriesClick = { series ->
